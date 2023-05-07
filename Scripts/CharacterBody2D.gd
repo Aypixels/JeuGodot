46168b874f915835e5f6307f2eaf61f301e6bac0
Aypixels <83863116+Aypixels@users.noreply.github.com>
2023-05-07 22:11:21 +0200

    tyzveirhm

--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 var speed = 0;
 var oldPose = "Down"
 
-
 func _physics_process(_delta):
 	velocity.x = (int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))) 
 	velocity.y = (int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))) 
@@ -31,3 +30,12 @@ func _physics_process(_delta):
 	else:
 		speed = 70
 		$AnimatedSprite2D.speed_scale = 1
+
+
+func _on_area_2d_body_entered(_body):
+	var gate = get_parent().get_node("Gate/AnimatedSprite2D")
+	gate.play("gate")
+
+func _on_area_2d_body_exited(_body):
+	var gate = get_parent().get_node("Gate/AnimatedSprite2D")
+	gate.stop()

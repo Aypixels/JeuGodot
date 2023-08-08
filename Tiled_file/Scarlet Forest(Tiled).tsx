dc6e62b5d0425b68e3b9d1f2489612254cc57113
ITake44 <guilletpaulin44@orange.fr>
2023-08-08 19:34:18 +0200

    Début de la wrap zone

---
+++ Tiled_file/Scarlet Forest(Tiled).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Scarlet Foret(Tiled)" tilewidth="20" tileheight="20" spacing="1" tilecount="264" columns="24">
+ <image source="../tilset/PC Computer - Deltarune - Scarlet Forest.png" trans="c386ff" width="516" height="230"/>
+</tileset>
---
+++ assets/battle_backgrounds/Closed door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Closed door" tilewidth="20" tileheight="20" tilecount="56" columns="8">
+ <image source="../../Tiled_file/Closed Gand door.png" trans="ff66ff" width="170" height="155"/>
+</tileset>

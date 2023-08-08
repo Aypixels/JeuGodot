b9dc0f025718851898ebe28d5da526a2ad2a65b8
ITake44 <guilletpaulin44@orange.fr>
2023-08-08 20:36:08 +0200

    Allez on y va

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -56,6 +56,7 @@ func combat_won() :
 
 
 func on_go_to_castle() :
+	fondu.play("fondu")
 	current_level = load("res://Scène/Castle.tscn").instantiate()
 	$Level/Tutoriel.queue_free()
 	$Level.add_child(current_level)

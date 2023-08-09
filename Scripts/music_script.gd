1e8091b73a03f1faa10e48ec93edd58ec2170d9f
ITake44 <guilletpaulin44@orange.fr>
2023-08-09 08:36:37 +0200

    Ajout musique + transi entre musique

--- Scripts/music_script.gd
+++ Scripts/music_script.gd
@@ -1,16 +1,25 @@
-extends AudioStreamPlayer
+extends Node
 
+@onready var music = $scarlet_forest
 
 func _ready():
 	SignalBus.fight.connect(cut_sound)
 	SignalBus.victory.connect(recover_sound)
-	stream_paused= false
+	music.stream_paused= false
+	SignalBus.memory.connect(switch_music)
 
 func cut_sound(_a, _b) :
-	stream_paused = true
+	music.stream_paused = true
 func recover_sound():
-	stream_paused = false
+	music.stream_paused = false
 
 func _process(_delta):
-	if playing == false and stream_paused == false :
-		play()
+	if music.playing == false and music.stream_paused == false :
+		music.play()
+
+func switch_music() :
+	while music.volume_db > -100 :
+		music.volume_db -= 1
+		await get_tree().create_timer(0.02).timeout
+	music.stream_paused = true
+	music = $castle_town
